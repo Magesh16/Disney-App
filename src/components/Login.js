52dda89b87263f217209ceec404fd5e4debaaa54
Magesh <mageshsk16@gmail.com>
2021-06-05 12:43:46 +0530

    Disney-react

---
+++ src/components/Login.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from "styled-components";
+
+const Login = (props)=>{
+    return(
+    <Container>
+        <Content>
+            content
+        </Content>
+    </Container>
+    )}
+
+const Container = styled.section`
+overflow :hidden;
+`
+const Content= styled.div`
+
+`
+
+export default Login;
