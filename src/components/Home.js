7548e28d192a963c39754d9af67c74d8cbe2fcb5
Magesh <mageshsk16@gmail.com>
2021-06-07 15:18:36 +0530

    setup Recommends,Originals,NewDisney..

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,19 @@
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
 import Recommends from "./Recommends";
+import NewDisney from "./NewDisney";
 import Viewers from "./Viewers";
+import Originals from "./Originals";
+import Trending from "./Trending";
 const Home =(props)=>{
     return(
     <Container>
         <ImgSlider/>
         <Viewers/>
         <Recommends/>
+        <NewDisney/>
+        <Originals/>
+        <Trending/>
     </Container>
     )}
 
